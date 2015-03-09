a5fed6147b8440b1a2f1c21a4182e6123db7d037
Guillaume <hi@guillaumemonnet.com>
2015-03-09 15:28:02 +0100

    reducing summary < 100char

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
     name: '255kb:meteor-status',
     version: '1.0.0',
-    summary: 'Meteor Status is a package which automatically alerts users when the connection to the server has been lost.',
+    summary: 'Meteor Status automatically alerts users when the connection to the server has been lost.',
     git: 'https://github.com/255kb/meteor-status',
     documentation: 'README.md'
 });
