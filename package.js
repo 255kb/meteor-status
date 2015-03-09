b8c0077d4950708af9667bb685b32a19e6e11d76
Guillaume <hi@guillaumemonnet.com>
2015-03-09 16:25:56 +0100

    manual insertion of the template

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: '255kb:meteor-status',
-    version: '1.0.0',
+    version: '1.1.0',
     summary: 'Meteor Status automatically alerts users when the connection to the server has been lost.',
     git: 'https://github.com/255kb/meteor-status',
     documentation: 'README.md'
@@ -9,5 +9,5 @@ Package.describe({
 Package.onUse(function(api) {
     api.versionsFrom('1.0');
     api.use(['templating', 'session', 'tracker', 'jquery'], 'client');
-    api.addFiles(['client/meteor-status.html', 'client/meteor-status.js', 'client/meteor-status.css', 'client/body.html'], 'client');
+    api.addFiles(['client/meteor-status.html', 'client/meteor-status.js', 'client/meteor-status.css'], 'client');
 });
