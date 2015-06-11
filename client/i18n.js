0258b8becf3f0bbdc162ad450dfca8cfaba38cab
蹬 <qdsang@gmail.com>
2015-06-11 18:13:55 +0800

    Update i18n.js

--- client/i18n.js
+++ client/i18n.js
@@ -6,5 +6,9 @@ meteorStatusI18n = {
     fr: {
         disconnected: 'Déconnecté du serveur, prochaine tentative de reconnexion dans %delay%s.',
         retry: 'Réessayer'
+    },
+    zh: {
+        disconnected: '从服务器断开连接，%delay%秒后将尝试重新链接。',
+        retry: '现在再试'
     }
 };
