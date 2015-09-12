a85df82f81d914889f2cd7edb4f8fba9eb3ab75b
Emad Shaaban <emad.shaaban92@gmail.com>
2015-09-12 15:32:59 +0200

    Add Arabic Support

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: '255kb:meteor-status',
-    version: '1.4.1',
+    version: '1.4.2',
     summary: 'Meteor Status automatically alerts users when the connection to the server has been lost.',
     git: 'https://github.com/255kb/meteor-status',
     documentation: 'README.md'
